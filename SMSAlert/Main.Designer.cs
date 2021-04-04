e142cdbffd1ada984f034456221a30bea7d7d658
Rudy Menjivar <ram302@nyu.edu>
2021-04-04 15:01:45 -0400

    Add project files.

---
+++ SMSAlert/Main.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace SMSAlert
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SMS_Input = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SMS_Input
+            // 
+            this.SMS_Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SMS_Input.Location = new System.Drawing.Point(31, 37);
+            this.SMS_Input.Margin = new System.Windows.Forms.Padding(5);
+            this.SMS_Input.Name = "SMS_Input";
+            this.SMS_Input.Size = new System.Drawing.Size(737, 49);
+            this.SMS_Input.TabIndex = 0;
+            this.SMS_Input.Text = "Enter your message here...";
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(567, 110);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(201, 44);
+            this.SendButton.TabIndex = 1;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 166);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.SMS_Input);
+            this.Name = "Main";
+            this.Text = "Welcome";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox SMS_Input;
+        private System.Windows.Forms.Button SendButton;
+    }
+}
+
---
+++ SMSAlert/Main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+using Twilio;
+using Twilio.Exceptions;
+using Twilio.Rest.Api.V2010;
+using System.Configuration;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace SMSAlert
+{
+    public partial class Main : Form
+    {
+        /*
+         * Set the following fields as environment variables in my system:
+         *      - RUN sysdm.cpl
+         *           - Advanced
+         *           - Environment Variables
+         *           - Added under System Variables
+         */
+        string TWILIO_SID = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        string TWILIO_AUTH = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        // Twilio provided a number; find in App.config
+        string TWILIO_PHONE = ConfigurationManager.AppSettings["TWILIO_PHONE"];
+
+        
+
+        public Main()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TwilioClient.Init(TWILIO_SID, TWILIO_AUTH);
+
+                var message = MessageResource.Create(
+                    body: SMS_Input.Text,
+                    from: new Twilio.Types.PhoneNumber(TWILIO_PHONE),
+                    to: new Twilio.Types.PhoneNumber("+16468314967")
+                );
+            }
+            catch(Twilio.Exceptions.ApiException ex)
+            {
+                // Implement a logger here?
+            }
+            finally
+            {
+                // Something to do in all cases here.
+            }
+        }
+    }
+}
